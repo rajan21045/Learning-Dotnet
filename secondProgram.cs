654ddf37b3ac822ac224aa8ac8b47cc52b19cda2
Rajan Poudel <raajan.works@gmail.com>
2026-02-24 18:09:57 +0545

    Add files via upload

---
+++ secondProgram.cs
@@ -0,0 +1,11 @@
+using System; // Provides basic functionalities like Console class
+
+class Program   // Main class of the application
+{
+    static void Main()   // Entry point of the program
+    {
+        int x = 12 * 30;  // Multiply 12 by 30 and store the result in variable x
+        
+        Console.WriteLine(x); // Display the value of x on the console
+    }
+}
