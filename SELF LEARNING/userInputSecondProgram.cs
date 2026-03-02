e7e06d7660cb9c031663218255dfc4ef7b7eb02b
Rajan Poudel <raajan.works@gmail.com>
2026-03-02 20:03:43 +0545

    Add files via upload

---
+++ SELF LEARNING/userInputSecondProgram.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter two numbers: ");
+        string input = Console.ReadLine();
+
+        string[] values = input.Split(' ');
+
+        int a = int.Parse(values[0]);
+        int b = int.Parse(values[1]);
+
+        Console.WriteLine("First Number: " + a);
+        Console.WriteLine("Second Number: " + b);
+    }
+}
