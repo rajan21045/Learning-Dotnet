927b8fe3662b5cc318dfd00f583e472d19a1d0bd
Rajan Poudel <raajan.works@gmail.com>
2026-03-08 18:18:04 +0545

    Add files via upload

---
+++ SELF LEARNING/LOOP/whileLoop.cs
@@ -0,0 +1,19 @@
+using System;
+namespace MyProgram{
+	class Program{
+		static void Main(string[] args){
+			int i = 0;
+			while(i<=5){
+				Console.WriteLine(i);
+				i++;
+			}
+			Console.WriteLine();
+			int j =1;
+			while(j<=5){
+				Console.WriteLine(j*2);
+				j++;
+			}
+	
+		}
+	}
+}
