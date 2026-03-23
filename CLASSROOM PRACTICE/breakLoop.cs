cf2efde26911a72b7aaf00fb0c6749dfa22f03be
Rajan Poudel <raajan.works@gmail.com>
2026-03-23 19:58:45 +0545

    Add files via upload

---
+++ CLASSROOM PRACTICE/breakLoop.cs
@@ -0,0 +1,16 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        for(int i=0; i<100; i++)
+        {
+            if (i == 10)
+            {
+                break;
+            }
+            Console.WriteLine("i: "+ i);
+        }
+        Console.WriteLine("Loop Complete");
+    }
+}
