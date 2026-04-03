491cff5d4305dbd47c01ae9098adb54f725cf97d
Rajan Poudel <raajan.works@gmail.com>
2026-04-03 20:12:47 +0545

    Update methodsProgram.cs

--- SELF LEARNING/METHODS/METHODS/methodsProgram.cs
+++ SELF LEARNING/METHODS/METHODS/methodsProgram.cs
@@ -5,7 +5,7 @@ A method is a block of code which only runs when it is called.
 You can pass data, known as parameters, into a method.
 
 Methods are used to perform certain actions, and they are also known as functions.
-
+ 
 Why use methods? To reuse code: define the code once, and use it many times.
 */
 
